cfd7f72f400c0a726222d028af3fcbbbfac28e7f
EmerGa <contact@linkify.fr>
2022-02-12 00:25:51 +0100

    :bug: Optimisation de l'utilisation des components

--- controller/components/README_controller-components.md
+++ controller/components/README_controller-components.md
@@ -1,34 +1,32 @@
 ## FICHIER ComponentsManager.php
+
 ComponentsManager permet le traitement des components dans les vues.
 
-
 # Utilisation des components
+
 Un component est une balise HTML personnalisée, qui, une fois traitée par Europa, est remplacée par un contenu prédéfini.
 
 Dans le markup, le component se définit comme suit :
-`
-<c:name attribute="value">
-	Contenu
-</c:name>
-`
+`<c:name attribute="value"> Contenu </c:name>`
 Chaque balise component doit être préfixée par `c:`. Elle peut comporter des attributs HTML (uniquement entre double guillemets). Elle peut être de type inline ou block.
 
 Il n'est pas nécessaire de déclarer l'ensemble des components utilisés. En revanche, une fonction de callback doit exister pour chaque component.
 
-- Si vous ne faites pas usage des components, il faut initialiser le Kernel dans public/europa.php en passant `"AUTOLOAD_COMPONENTS"=>false` dans les paramètres de la méthode __construct. Par défaut, Europa tentera de parser les components. S'il n'y en a pas, aucune exception ne sera levée.
-
+-   Si vous ne faites pas usage des components, il faut initialiser le Kernel dans public/europa.php en passant `"AUTOLOAD_COMPONENTS"=>false` dans les paramètres de la méthode \_\_construct. Par défaut, Europa tentera de parser les components. S'il n'y en a pas, aucune exception ne sera levée.
 
 # Définition des fonctions de parsing
-Les fonctions de callback doivent être crées dans des fichiers portant le nom du component. Ces fichiers doivent se trouver dans le dossier `controller/components/components`. Le nom de la fonction doit être préfixé par `component_` et déclarée dans l'espace de nom Europa\Vue\Components .
+
+Les fonctions de callback doivent être crées dans des fichiers portant le nom du component. Ces fichiers doivent se trouver dans le dossier `controller/components/components`. Le nom de la fonction doit être préfixé par `component_` et déclarée dans l'espace de nom `Europa\Vue\Components` .
 
 Attention : les fonctions de parsing doivent être des fonctions et non pas des classes. Il ne doit y avoit qu'une fonction par fichier (un fichier -> une fonction -> un component).
 
 Ainsi, pour le component `test`, la balise HTML prendra la forme `<c:test></c:test>` et la fonction correspondante sera créée dans le fichier `test.php` sous le nom `component_test`.
 
 Chaque fonction prend pour paramètre un tableau associatif. Ce tableau inclut plusieurs éléments :
-* `block` (string) contient l'ensemble de la balise telle que présente dans le markup.
-* `content` (string) contient le contenu de la balise si elle est de type block.
-* `name` (string) est le nom du component
-* `attributes` (array) est un tableau contenant les attributes (attribut => valeur)
 
-La fonction n'a pas besoin d'un `return`. Elle peut renvoyer du contenu HTML par la fonction `echo` ou en fermant la balise PHP (`?> markup... <?php`).
+-   `block` (string) contient l'ensemble de la balise telle que présente dans le markup.
+-   `content` (string) contient le contenu de la balise si elle est de type block.
+-   `name` (string) est le nom du component
+-   `attributes` (array) est un tableau contenant les attributes (attribut => valeur)
+
+La fonction a besoin d'un `return`. Elle ne doit pas renvoyer du contenu HTML par la fonction `echo` ou en fermant la balise PHP (`?> markup... <?php`), sans quoi le contenu renvoyé apparaitra avant tout le reste de la page.
--- vues/vueManager/VueManager.php
+++ vues/vueManager/VueManager.php
@@ -2,83 +2,89 @@
 
 namespace Europa\Vue;
 
-class VueManager {
-	
-	public $attributes = array();
-	public $URLattributes = array();
-	
-	public function __construct($fileName, $fileParameters) {
-		
-		if (!isset($fileParameters)) {
-			$fileParameters = array(
-				"TEMPLATE_EXTENSION"=>"php"
-			);
-		}
-		$this->attributes = $fileParameters;
-		unset($this->attributes["TEMPLATE_EXTENSION"]);
-		
-		$this->URLattributes = \Europa\Core\RouteHandler::$URLattributes;
-		
-		chdir("../");
-		$tagsDir = realpath("controller".DIRECTORY_SEPARATOR."components".DIRECTORY_SEPARATOR."components".DIRECTORY_SEPARATOR);
-		
-		chdir("public");
-		
-		if (\Europa\Core\Kernel::$OPTIONS['AUTOLOAD_COMPONENTS']) {
-			$components = new \Europa\Vue\ComponentsManager\ComponentsManager(array(
-				'tag_directory' 		=> $tagsDir,
-				'template_directory' 	=> $tagsDir
-			));
-		}
-		
-		chdir("../");
-		include("vues/templates/$fileName.".$fileParameters["TEMPLATE_EXTENSION"]);
-		
-		chdir("public");
-		
-		if (\Europa\Core\Kernel::$OPTIONS['AUTOLOAD_COMPONENTS']) {
-			$components->parse();
-		}
-	}
-	
-	public function include($vue, array $fileParameters = array(
-				"TEMPLATE_EXTENSION"=>"php"
-			)) {
-		//chdir("../");
-			include("vues/templates/$vue.".$fileParameters["TEMPLATE_EXTENSION"]);
-		
-		//chdir("public");
-	}
-	
-	public function loremIpsum(string $type = 'P', $length = 1) {
-		if ($type == 'P') {
-			$rtn = "<p>Do malis proident, dolore probant e pariatur. Nulla cernantur instituendarum te 
+class VueManager
+{
+
+    public $attributes = array();
+    public $URLattributes = array();
+
+    public function __construct($fileName, $fileParameters)
+    {
+
+        if (!isset($fileParameters)) {
+            $fileParameters = array(
+                "TEMPLATE_EXTENSION" => "php"
+            );
+        }
+        $this->attributes = $fileParameters;
+        unset($this->attributes["TEMPLATE_EXTENSION"]);
+
+        $this->URLattributes = \Europa\Core\RouteHandler::$URLattributes;
+
+        chdir("../");
+        $tagsDir = realpath("controller" . DIRECTORY_SEPARATOR . "components" . DIRECTORY_SEPARATOR . "components" . DIRECTORY_SEPARATOR);
+
+        chdir("public");
+
+        if (\Europa\Core\Kernel::$OPTIONS['AUTOLOAD_COMPONENTS']) {
+            $components = new \Europa\Vue\ComponentsManager\ComponentsManager(array(
+                'tag_directory'         => $tagsDir,
+                'template_directory'     => $tagsDir
+            ));
+        }
+
+        chdir("../");
+        include("vues/templates/$fileName." . $fileParameters["TEMPLATE_EXTENSION"]);
+
+        chdir("public");
+
+        if (\Europa\Core\Kernel::$OPTIONS['AUTOLOAD_COMPONENTS']) {
+            $components->parse();
+        }
+    }
+
+    public function include($vue, array $fileParameters = array(
+        "TEMPLATE_EXTENSION" => "php"
+    ))
+    {
+        //chdir("../");
+        include("vues/templates/$vue." . $fileParameters["TEMPLATE_EXTENSION"]);
+
+        //chdir("public");
+    }
+
+    public function loremIpsum(string $type = 'P', $length = 1)
+    {
+        if ($type == 'P') {
+            $rtn = "<p>Do malis proident, dolore probant e pariatur. Nulla cernantur instituendarum te 
 an magna amet se iudicem. Ab legam nostrud despicationes. Eram iudicem nam 
 voluptate ne nescius in sunt. Labore laborum cernantur. Qui voluptate 
 consectetur, incididunt de veniam, fore officia ex sempiternum, eram o appellat. 
 Labore de eiusmod ita tamen ex do de dolor deserunt qui aut o malis pariatur id 
 noster ut voluptate a dolor a aut te duis admodum, a tamen nescius distinguantur 
 ita sint quamquam doctrina do ex iis quem tamen magna.</p>";
-		} else if ($type == 'S') {
-			$rtn = "Incididunt imitarentur quo voluptate a do iis comprehenderit. ";
-		} else {
-			$rtn = "Quorum an incurreret nam multos id sed laborum reprehenderit. Quamquam eram amet o quorum, hic culpa ingeniis. De quid occaecat vidisse, quo legam irure cillum mandaremus. Quibusdam fidelissimae do fabulas nam aut senserit ne iudicem. Sint ut ab fugiat admodum, ab de tamen quorum quid. ";
-		}
-		echo str_repeat($rtn,$length);
-	}
-	
-	public function get($val) {
-		if (isset($this->attributes[$val])) {
-			return $this->attributes[$val];
-		} else {
-			return null;
-		}
-	}
-	public function getURLParameter($val) {
-		if (isset($this->URLattributes[$val])) {
-			return $this->URLattributes[$val];
-		} else {
-			return null;
-		}
-	}
+        } else if ($type == 'S') {
+            $rtn = "Incididunt imitarentur quo voluptate a do iis comprehenderit. ";
+        } else {
+            $rtn = "Quorum an incurreret nam multos id sed laborum reprehenderit. Quamquam eram amet o quorum, hic culpa ingeniis. De quid occaecat vidisse, quo legam irure cillum mandaremus. Quibusdam fidelissimae do fabulas nam aut senserit ne iudicem. Sint ut ab fugiat admodum, ab de tamen quorum quid. ";
+        }
+        echo str_repeat($rtn, $length);
+    }
+
+    public function get($val)
+    {
+        if (isset($this->attributes[$val])) {
+            return $this->attributes[$val];
+        } else {
+            return null;
+        }
+    }
+    public function getURLParameter($val)
+    {
+        if (isset($this->URLattributes[$val])) {
+            return $this->URLattributes[$val];
+        } else {
+            return null;
+        }
+    }
 }
